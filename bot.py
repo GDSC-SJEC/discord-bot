92725a5c6c09ee19d7ae9109cf8f5976b26491ca
pravin-pk <19cs119.pravin@sjec.ac.in>
2022-08-26 16:15:39 +0530

    Integrated extra commands

--- bot.py
+++ bot.py
@@ -1,3 +1,4 @@
+from unicodedata import name
 import discord
 from discord.ext import commands
 from discord.utils import get
@@ -29,45 +30,61 @@ class MyClient(commands.Bot):
 
 
 
-client = MyClient()
-
+bot = MyClient()
+domains = ['Web Development', 'Mobile App Development', 'Game Development', 'AI/ML', 'Cloud Computing', 'Competitive Programming',  'UI/UX']
 
 
 # bot = commands.Bot(command_prefix='!', intents = discord.Intents.default())
 
-@client.command()
+@bot.hybrid_command(name = 'ping', with_app_command = True)
+async def ping(ctx):
+    await ctx.reply('Pong!')
+
+@bot.command()
 async def test(ctx, *args):
     # print("firing this")
     arguments = ', '.join(args)
-    await ctx.send(f'{len(args)} arguments: {arguments}')
+    await ctx.reply(f'{len(args)} arguments: {arguments}')
 
-@client.command()
+@bot.command()
 async def join(ctx):
-    await ctx.send('Which Domain you wanna join?\n 1. Web Development\n 2. Mobile App Development\n 3. Game Development\n 4. AI/ML\n 5. Cloud Computing\n 6. Competitive Programming\n 7. UI/UX\n 8.Others')
+    await ctx.reply(f'Which Domain you wanna join?\n 1. Web Development\n 2. Mobile App Development\n 3. Game Development\n 4. AI/ML\n 5. Cloud Computing\n 6. Competitive Programming\n 7. UI/UX\n 8.Others')
+    await ctx.send('```Use !joingdsc [domain index] command to join any of the above domains```')
 
-@client.command()
+@bot.command()
+async def joingdsc(ctx, index):
+    try:
+        if index == '8':
+            await ctx.reply('Contact Admin for Assistance')
+            return
+        domain = domains[int(index)-1]
+        await ctx.reply(f'Congratulations 🎉🎊,\nYou have Succesfully joined {domain}')
+    except IndexError:
+        await ctx.reply(f'Invalid Domain Index\n ＞︿＜')
+
+@bot.command()
 async def gdsc(ctx):
-    await ctx.send('```Avilable Commands are: \n1. !test\n2. !join\n3. !gdsc```')
+    await ctx.reply('Avilable Commands are: \n```1. !test\n2. !join\n3. !gdsc```')
 
 
-@client.event
+@bot.event
 async def on_message(message):
-    if message.author == client.user:
+    if message.author == bot.user:
         return
 
     if 'hello' in message.content.lower():
-        await message.channel.send(f'Hello {message.author.name} (👉ﾟヮﾟ)👉,\n Wassup, You can type !gdsc to see the available commands')
+        await message.channel.send(f'Hello {message.author.name} (👉ﾟヮﾟ)👉,\n Wassup, You can type **!gdsc** to see the available commands')
 
     # if message.content.startswith('$join'):
         # await message.channel.send('Which Domain you wanna join?\n 1. Web Development\n 2. Mobile App Development\n 3. Game Development\n 4. AI/ML\n 5. Cloud Computing\n 6. Competitive Programming\n 7. UI/UX\n 8.Others')
     
-    await client.process_commands(message)
+    await bot.process_commands(message)
 
-@client.event
+@bot.event
 async def on_message_delete(message):
     await message.channel.send(f'Yo {message.author.name}, Admin saw what you deleted 😏')
 
-@client.event 
+@bot.event 
 async def on_member_join(member):
     print(member.roles)
     await member.send(f'Welcome {member.name} to the GDSC Discord Server 🥳')
@@ -75,4 +92,4 @@ async def on_member_join(member):
 
 
 
-client.run(os.environ['token'])
+bot.run(os.environ['token'])
