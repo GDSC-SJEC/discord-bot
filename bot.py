524d4f61026ef7b343fadbcc2ab17670e8d16a77
pravin-pk <19cs119.pravin@sjec.ac.in>
2022-08-27 22:07:43 +0530

    bug fixes

--- bot.py
+++ bot.py
@@ -18,7 +18,7 @@ else:
 class MyClient(commands.Bot):
 
     def __init__(self) -> None:
-        super().__init__(command_prefix='!', intents = discord.Intents.all())
+        super().__init__(command_prefix='~', intents = discord.Intents.all())
         self.message_content = True
         self.members = True
         # self.guilds = True
@@ -27,6 +27,7 @@ class MyClient(commands.Bot):
         print('Logged in as', self.user)
 
     async def on_command_error(self, ctx, error):
+        print(error)
         await ctx.reply(error, ephemeral=True)
 
 
@@ -35,40 +36,47 @@ class MyClient(commands.Bot):
 
 bot = MyClient()
 domains = ['Web Development', 'Mobile App Development', 'Game Development', 'AI/ML', 'Cloud Computing', 'Competitive Programming',  'UI/UX']
+roles = ['WEB', 'MOBILE', 'GAME', 'AI/ML', 'CLOUD', 'COMPETITIVE', 'UI/UX']
 
 
 # bot = commands.Bot(command_prefix='!', intents = discord.Intents.default())
 
-@bot.hybrid_command(name = 'ping', with_app_command = True)
-async def ping(ctx):
-    await ctx.reply('Pong!')
+@bot.hybrid_command(name = 'pinging', with_app_command = True)
+async def pinging(ctx):
+    await ctx.reply('Pong~')
 
 @bot.command()
 async def test(ctx, *args):
     # print("firing this")
     arguments = ', '.join(args)
-    print(args[5])
+    # print(arguments[5])
     await ctx.reply(f'{len(args)} arguments: {arguments}')
 
 @bot.command()
 async def join(ctx):
     await ctx.reply(f'Which Domain you wanna join?\n 1. Web Development\n 2. Mobile App Development\n 3. Game Development\n 4. AI/ML\n 5. Cloud Computing\n 6. Competitive Programming\n 7. UI/UX\n 8.Others')
-    await ctx.send('```Use !joingdsc [domain index] command to join any of the above domains```')
+    await ctx.send('```Use ~joingdsc [domain index] command to join any of the above domains```')
 
-@bot.command()
+@bot.command(pass_context=True)
+# @commands.has_role("GDSC-SJEC") 
 async def joingdsc(ctx, index):
     try:
         if index == '8':
             await ctx.reply('Contact Admin for Assistance')
             return
-        domain = domains[int(index)-1]
+        domain = domains[int(index)-1]   
+        
+        member = ctx.author
+        var = discord.utils.get(ctx.guild.roles, name = roles[int(index)-1])
+        await member.add_roles(var)
+
         await ctx.reply(f'Congratulations 🎉🎊,\nYou have Succesfully joined {domain}')
     except IndexError:
         await ctx.reply(f'Invalid Domain Index\n ＞︿＜')
 
 @bot.command()
 async def gdsc(ctx):
-    await ctx.reply('Avilable Commands are: \n```1. !test\n2. !join\n3. !gdsc```')
+    await ctx.reply('Avilable Commands are: \n```1. ~test\n2. ~join\n3. ~gdsc \n4. ~ping```')
 
 
 @bot.event
@@ -77,7 +85,7 @@ async def on_message(message):
         return
 
     if 'hello' in message.content.lower():
-        await message.channel.send(f'Hello {message.author.name} (👉ﾟヮﾟ)👉,\n Wassup, You can type **!gdsc** to see the available commands')
+        await message.channel.send(f'Hello {message.author.name} (👉ﾟヮﾟ)👉,\n Wassup, You can type **~gdsc** to see the available commands')
 
     # if message.content.startswith('$join'):
         # await message.channel.send('Which Domain you wanna join?\n 1. Web Development\n 2. Mobile App Development\n 3. Game Development\n 4. AI/ML\n 5. Cloud Computing\n 6. Competitive Programming\n 7. UI/UX\n 8.Others')
