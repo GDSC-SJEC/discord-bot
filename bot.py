f5812d2e11cabbf634fc9aacc424ac070cb6bea7
pravin-pk <19cs119.pravin@sjec.ac.in>
2022-09-12 00:13:21 +0530

    bot release v0.1

--- bot.py
+++ bot.py
@@ -52,8 +52,7 @@ class verify_button_view(discord.ui.View):
         button.emoji = "✅"
         button.disabled = True
         await interaction.response.edit_message(view=self)
-        await interaction.followup.send(f"Thank you {interaction.user.mention}, You are now a verified user✅\n ```Now you can use ~joingdsc command to join your domain of interest!```")
-        # await discord.Interaction.response.send_message(f"Thank you {interaction.user}, You are now a verified user✅\n ```Now you can use ~joingdsc command to join your domain of interest!```")
+        await interaction.followup.send(f"Thank you {interaction.user.mention}, You are now a verified user✅")
 
 
 class form_button_view(discord.ui.View):
@@ -148,15 +147,14 @@ bot = MyClient()
 # tree = app_commands.CommandTree(bot)
 
 domains = ['Web Development', 'Mobile App Development', 'Game Development', 'AI/ML', 'Cloud Computing', 'Competitive Programming',  'UI/UX']
-roles = ['WEB', 'MOBILE', 'GAME', 'AI/ML', 'CLOUD', 'COMPETITIVE', 'UI/UX']
 roles = {
             "Web Development": "WEB",
-            "Mobile App Development": "MOBILE",
+            "Android Development": "ANDROID",
             "Game Development": "GAME",
-            "AI/ML": "AI/ML",
+            "AI/ML": "AI_ML",
             "Cloud Computing": "CLOUD",
             "Competitive Programming": "COMPETITIVE",
-            "UI/UX Design": "UI/UX"
+            "UI/UX": "UI_UX"
         }
 
 
@@ -230,18 +228,30 @@ async def on_message(message):
     if message.author == bot.user:
         return
 
-    # if 'hello' in message.content.lower() or 'hi' in message.content.lower():
-    if message.content.lower().startswith('hi'):
-        view = form_button_view()
-        await message.author.send(view = view)
+    if message.channel.id == int(os.environ['introductionChannelId']):
+        try:
+            if len(message.content) < 20:
+                await message.author.send('Please write a proper introduction!')
+                raise Exception
+            reacted = discord.utils.get(message.guild.roles, name = 'verified')
+            await message.author.add_roles(reacted)
+            await message.add_reaction('✅')
+        except Exception as e:
+            print(e)
+            await message.add_reaction('❌')
+
+
+    if message.content.lower().startswith('dm'):
+        await message.author.send(view = form_button_view())
+        # await bot.get_channel(1010434735757082706).send(view = form_button_view())
         await message.channel.send(f'Hello {message.author.mention} (👉ﾟヮﾟ)👉,\n Wassup\n Ive slided into your DMs, catch me there! 🤗')
-        embed = discord.Embed(color = 0xffffff, title = 'The Process Flow', description = "Here's the step by step guide on what to do next")
-        embed.add_field(name="Step 1", value="Check you DM for message from GDSC-SJEC BOT", inline=False)
-        embed.add_field(name="Step 2", value="Click on the Button to store your info into our Database", inline=False)
-        embed.add_field(name="Step 3", value="Go to _Introduction_  Channel and introduce yourself", inline=False)
-        embed.add_field(name="Step 4", value="Come back to verification Channel and use ~join command", inline=False)
-        # embed.add_field(name="You are good to go now!", value = '', inline=False)
-        await message.author.send(embed = embed)
+        # embed = discord.Embed(color = 0xffffff, title = 'The Process Flow', description = "Here's the step by step guide on what to do next")
+        # embed.add_field(name="Step 1", value="Check you DM for message from GDSC-SJEC BOT", inline=False)
+        # embed.add_field(name="Step 2", value="Click on the Button to store your info into our Database", inline=False)
+        # embed.add_field(name="Step 3", value="Go to _Introduction_  Channel and introduce yourself", inline=False)
+        # embed.add_field(name="Step 4", value="Come back to verification Channel and use ~join command", inline=False)
+        # # embed.add_field(name="You are good to go now!", value = '', inline=False)
+        # await message.author.send(embed = embed)
 
     if message.content.lower().startswith('domains'):
         # embed = discord.Embed(color = 0xff00ff, title = 'Web Development', description = "Web Dev is sexy profession")
@@ -251,33 +261,74 @@ async def on_message(message):
 
         domains = [
             {
-                'color' : 0xffffff,
-                'title' : 'Web Development',
-                'description' : 'Game Development is also sexy',
-                'footer' : {'text' : 'For more information contact the domain lead by clicking the name above'},
-                'image' : {'url' : 'https://picsum.photos/800/200?random=2', 'height' : 200, 'width' : 800},
-                'author' : {'name' : 'ashish', 'url' : 'https://www.google.com', 'icon_url' : 'https://www.google.com'}
+            'color': 0x064f9a,
+            'title': 'Web Development',
+            'description': 'The web is our playground, come be a part of the team that shapes the web',
+            'footer' : {'text': 'For more information contact the domain lead by clicking the name above'},
+            'image' : {'url': 'https://wallpaperaccess.com/full/1657858.jpg', 'height': 200, 'width': 800},
+            'author' : {'name': 'Godnon', 'url': 'https://www.linkedin.com/in/godnon-dsilva/', 'icon_url': 'https://avatars.githubusercontent.com/u/50076340?v=4'}
             },
             {
             'color' : 0xffffff,
             'title' : 'Game Development',
             'description' : 'An art of developing a virtual world which models by the rules and constraints defined by you.',
             'footer' : {'text' : 'If you want to know more about Game Dev or have some doubts , you can always ping me by clicking on my name!'},
-            'image' : {'url' : 'https://imgur.com/b3CirLf', 'height' : 200, 'width' : 800},
-            'author' : {'name' : 'Ashish Kishore', 'url' : 'https://www.linkedin.com/in/ashish-kishore-kumar/', 'icon_url' : 'https://imgur.com/a/SkEI7kU' }
+            'image' : {'url' : 'https://images.unsplash.com/photo-1556438064-2d7646166914?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80', 'height' : 200, 'width' : 800},
+            'author' : {'name' : 'Ashish Kishore', 'url' : 'https://www.linkedin.com/in/ashish-kishore-kumar/', 'icon_url' : 'https://avatars.githubusercontent.com/u/68268366?v=4' }
             },
             {
-                'color' : 0xff09ff,
-                'title' : 'Mobile App Development',
-                'description' : 'Game Development is also sexy',
-                'footer' : {'text' : 'For more information contact the domain lead by clicking the name above'},
-                'image' : {'url' : 'https://picsum.photos/800/200?random=2', 'height' : 200, 'width' : 800},
-                'author' : {'name' : 'ashish', 'url' : 'https://www.google.com', 'icon_url' : 'https://www.google.com'}
+            'color' : 0xffffff,
+            'title' : 'Android Development',
+            'description' : 'Android development mainly focuses on the development of android applications using kotlin and flutter.',
+            'footer' : {'text' : 'For more information contact the domain lead by clicking the name above'},
+            'image' : {'url' : 'https://media.discordapp.net/attachments/1017871199654658099/1018025844976910386/Android_Image.jpg', 'height' : 200, 'width' : 800},
+            'author' : {'name' : 'Pratheek', 'url' : 'https://www.linkedin.com/in/pratheek-raghunath/', 'icon_url' : 'https://media.discordapp.net/attachments/776130355681427498/1018026587163213864/Dp.jpeg'}
+            },
+            {
+            'color' : 0x000000,
+            'title' : 'AI/ML',
+            'description' : 'Artificial Intelligence: Where machines are smarter',
+            'footer' : {'text' : 'Contact me by clicking on my name, Thank you 🤗'},
+            'image' : {'url' : 'https://cdn.discordapp.com/attachments/1013104220934787152/1018577533609975819/unknown.png', 'height' : 200, 'width' : 800},
+            'author' : {'name' : 'Pravin Kumar', 'url' : 'https://cdn.discordapp.com/attachments/1013104220934787152/1018577280332746752/20220812_221553.jpg'}
+            },
+            {
+            'color' : 0xA5DEF2,
+            'title' : 'Cloud Computing',
+            'description' : 'Delivery of computing services—including servers, storage, databases, networking, software, analytics, and intelligence—over the Internet',
+            'footer' : {'text' : 'For more information contact the domain lead by clicking the name above'},
+            'image' : {'url' : 'https://media.geeksforgeeks.org/wp-content/cdn-uploads/20200710234318/Why-Cloud-Computing-is-Booming-in-the-Market.png', 'height' : 200, 'width' : 800},
+            'author' : {'name' : 'Oren Saldanha', 'url' : 'https://www.linkedin.com/in/orensaldanha/', 'icon_url' : 'https://avatars.githubusercontent.com/u/36854536?v=4'}
+            },
+            {
+            "color": 0xEDFF71,
+            "title": "Competitive Programming",
+            "description": "Think, Analyze, Solve Problems and Unravel a new world of Puzzles!",
+            "footer": {"text": "Feel free to contact me by clicking on my name above"},
+            "image": {
+                "url": "https://images.unsplash.com/photo-1454165804606-c3d57bc86b40?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+                "height": 200,
+                "width": 800
+            },
+            "author": {
+                "name": "Yashas",
+                "url": "https://www.linkedin.com/in/yashas-ip-4926641b4/",
+                "icon_url": "https://firebasestorage.googleapis.com/v0/b/gdsc-sjec.appspot.com/o/team%2F4SO19CS180.jpg?alt=media&token=e3c6e892-1def-45e5-9e08-dacc932d6e46"
+            }
+            }, 
+            {
+            'color': 0xf01620,
+            'title': 'UI/UX',
+            'description': 'Design wonderful and exciting interfaces and experiences for users',
+            'footer' : {'text': 'For more information contact the domain lead by clicking the name above'},
+            'image' : {'url': 'https://images.unsplash.com/photo-1658204212985-e0126040f88f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1162&q=80', 'height': 200, 'width': 800},
+            'author' : {'name': 'Avinash', 'url': 'https://www.linkedin.com/in/avinash-muralidharan-75b1a9226/', 'icon_url': 'https://cdn.discordapp.com/avatars/478525019157299200/c51b5b9b8541d03c4d3d36c410179ccf.webp?size=160%27%7D'}
             }
         ]
+        channel = bot.get_channel(int(os.environ['domainChannelId']))
         for dic in domains:
             embed = discord.Embed().from_dict(dic)
-            await message.channel.send(embed = embed)
+            await channel.send(embed = embed)
 
 
     # if message.content.startswith('$join'):
@@ -287,20 +338,23 @@ async def on_message(message):
 
 @bot.event
 async def on_message_delete(message):
-    await message.channel.send(f'Yo {message.member.name}, Admin saw what you deleted 😏')
+    await message.channel.send(f'Yo {message.user.mention}, Admin saw what you deleted 😏')
 
 @bot.event 
 async def on_member_join(member):
     print(member.roles)
+    await member.send(view = form_button_view())
     await member.send(f'Welcome {member.mention} to the GDSC Discord Server 🥳\n We are delighted to Have you here')
 
 @bot.event
 async def on_reaction_add(reaction, user):
-    if reaction.message.channel.name == 'testing':
+    if reaction.message.channel.id == int(os.environ['domainChannelId']) and 'verified' in [role.name.lower() for role in user.roles]:
         try:
-            reacted = discord.utils.get(reaction.message.guild.roles, name = roles[reaction.message.embeds[0].title])
+            title = reaction.message.embeds[0].title
+            reacted = discord.utils.get(reaction.message.guild.roles, name = roles[title])
             await user.add_roles(reacted)
-            await user.send(f"You are now a part of the {reaction.message.embeds[0].title} Domain of GDSC-SJEC community 🥳")
+            await user.send(f"You are now a part of the {title} Domain of GDSC-SJEC community 🥳")
+            await bot.get_channel(int(os.environ[roles[title]])).send(f"{user.mention} has joined the {title} Domain of GDSC-SJEC community 🥳")
         except IndexError:
             await user.send("Oh Sorry!, You reacted to wrong message 😕")
 
